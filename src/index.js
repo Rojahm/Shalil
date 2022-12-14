af62f36371637f3906a2097b1f8be3cb613d2c35
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-12-14 13:30:10 +0330

    few fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-let rand1 = document.querySelector("#rand1");
-let rand2 = document.querySelector("#rand2");
-let rand3 = document.querySelector("#rand3");
+let box1 = document.querySelector("#box1");
+let box2 = document.querySelector("#box2");
+let box3 = document.querySelector("#box3");
 let img = document.querySelector("#random-img");
 let luckBtn = document.querySelector("#lucky-button");
 let resetBtn = document.querySelector("#reset-button");
@@ -49,15 +49,15 @@ function randomNumber(max) {
 }
 function resetSlot(event) {
   event.preventDefault();
-  for (let box of boxes) {
-    box.classList.remove("d-none");
-  }
-
   resetBtn.classList.add("d-none");
   luckBtn.classList.remove("d-none");
-  // rand1.classList.add("d-none");
-  // rand2.classList.add("d-none");
-  // rand3.classList.add("d-none");
+  // for (let box of boxes) {
+  //   box.classList.remove("d-none");
+  // }
+
+  // box1.classList.add("d-none");
+  // box2.classList.add("d-none");
+  // box3.classList.add("d-none");
 
   for (let inside of insides) {
     inside.classList.add("d-none");
@@ -76,17 +76,17 @@ function randomEmoji(event) {
     inside.classList.add("active");
   }
 
-  let contan1 = [""];
-  let contan2 = [];
-  let contan3 = [];
+  let contan1 = "";
+  let contan2 = "";
+  let contan3 = "";
   for (let index = 0; index < 6; index++) {
     contan1 += emojis[randomNumber(maxNumber)];
     contan2 += emojis[randomNumber(maxNumber)];
     contan3 += emojis[randomNumber(maxNumber)];
   }
-  rand1.innerHTML = contan1;
-  rand2.innerHTML = contan2;
-  rand3.innerHTML = contan3;
+  box1.innerHTML = contan1;
+  box2.innerHTML = contan2;
+  box3.innerHTML = contan3;
 
   document.querySelector("#box3").addEventListener("transitionend", () => {
     console.log("ended");
@@ -95,9 +95,9 @@ function randomEmoji(event) {
   img.classList.remove("d-none");
   luckBtn.classList.add("d-none");
   resetBtn.classList.remove("d-none");
-  rand1.classList.remove("d-none");
-  rand2.classList.remove("d-none");
-  rand3.classList.remove("d-none");
+  box1.classList.remove("d-none");
+  box2.classList.remove("d-none");
+  box3.classList.remove("d-none");
 }
 
 luckBtn.addEventListener("click", randomEmoji);
